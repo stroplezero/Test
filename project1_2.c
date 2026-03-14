76a17f4822c18e5affa6ba413dd3ea31dd651b24
stroplezero <a01039072925@gmail.com>
2026-03-14 21:08:23 +0900

    Add binary conversion for double values
    Implement binary conversion for double input with handling for negative values.

--- project1_2.c
+++ project1_2.c
@@ -1,1 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+    int main() {
+        double input;
+        int bits[16] = { 0 }; 
+
+        printf("입력값: ");
+        scanf("%lf", &input);
+
+        double temp = (input < 0) ? -input : input;
+        int i_part = (int)temp;
+        double f_part = temp - i_part;
+
+        for (int i = 7; i >= 0; i--) {
+            bits[i] = i_part % 2;
+            i_part /= 2;
+        }
+
+        for (int i = 8; i < 16; i++) {
+            f_part *= 2;
+
+            if (f_part >= 1) {
+                bits[i] = 1;
+                f_part -= 1;
+            }
+            else {
+                bits[i] = 0;
+            }
+        }
+
+        if (input < 0) {
+           
+            for (int i = 0; i < 16; i++) {
+                bits[i] = (bits[i] == 0) ? 1 : 0;
+            }
+
+            for (int i = 15; i >= 0; i--) {
+                if (bits[i] == 0) {
+                    bits[i] = 1; 
+                    break;
+                }
+                else {
+                    bits[i] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < 16; i++) {
+            printf("%d", bits[i]);
+            if (i == 7) printf("."); 
+        }
+
+        return 0;
+    }
